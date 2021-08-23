f8bbca0cb2136a40347c6948d0ceb73de671da7b
Tyler Knight <TylerKnight2197@gmail.com>
2021-08-23 18:49:10 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var likes=9
+var likesElement = document.querySelector("#likes")
+
+function liked() {
+    likes++
+    likesElement.innerText = likes + " like(s)"
+}
+
+var likes1=12
+var likes1Element = document.querySelector("#likes1")
+
+function liked1() {
+    likes1++
+    likes1Element.innerText = likes1 + " like(s)"
+}
+
+var likes2=9
+var likes2Element = document.querySelector("#likes2")
+
+function liked2() {
+    likes2++
+    likes2Element.innerText = likes2 + " like(s)"
+}
